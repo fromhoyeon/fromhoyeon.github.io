3f85ce749a587ea0ee5bba462c1acdccfc9ce172
fromhoyeon <fromhoyeon@gmail.com>
2026-09-02 16:46:29 +0900

    Register Sanity schema types

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import {siteCopy} from './siteCopy'
+import {portfolioPhoto} from './portfolioPhoto'
+import {contentEntry} from './contentEntry'
+
+export const schemaTypes = [siteCopy, portfolioPhoto, contentEntry]
