0844deda9746157b4c90a5eb69e87f68a3920c9e
fromhoyeon <fromhoyeon@gmail.com>
2026-09-03 16:52:33 +0900

    Register work content block schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,25 @@
 import {siteCopy} from './siteCopy'
 import {portfolioPhoto} from './portfolioPhoto'
 import {contentEntry} from './contentEntry'
+import {
+  workGalleryBlock,
+  workGalleryImage,
+  workTextBlock,
+  workVideoBlock,
+  workWebEmbedBlock,
+} from './workContentBlocks'
 import {workEntry} from './workEntry'
 import {homePage} from './homePage'
 
-export const schemaTypes = [siteCopy, portfolioPhoto, contentEntry, workEntry, homePage]
+export const schemaTypes = [
+  siteCopy,
+  portfolioPhoto,
+  contentEntry,
+  workVideoBlock,
+  workTextBlock,
+  workGalleryImage,
+  workGalleryBlock,
+  workWebEmbedBlock,
+  workEntry,
+  homePage,
+]
