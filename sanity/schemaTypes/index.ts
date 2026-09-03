9a916d2bccda8371ec73dde36bf35190658d33f1
fromhoyeon <fromhoyeon@gmail.com>
2026-09-03 18:00:52 +0900

    Register primary navigation reference schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import {siteCopy} from './siteCopy'
+import {siteNavigation} from './siteNavigation'
 import {portfolioPhoto} from './portfolioPhoto'
 import {contentEntry} from './contentEntry'
 import {
@@ -13,6 +14,7 @@ import {homePage} from './homePage'
 
 export const schemaTypes = [
   siteCopy,
+  siteNavigation,
   portfolioPhoto,
   contentEntry,
   workVideoBlock,
