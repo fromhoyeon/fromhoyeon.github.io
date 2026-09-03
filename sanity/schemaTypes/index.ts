5c0e94ba2455451c82ab1ea22b4146464182a3c8
fromhoyeon <fromhoyeon@gmail.com>
2026-09-03 11:43:12 +0900

    Register homepage work schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import {siteCopy} from './siteCopy'
 import {portfolioPhoto} from './portfolioPhoto'
 import {contentEntry} from './contentEntry'
+import {workEntry} from './workEntry'
+import {homePage} from './homePage'
 
-export const schemaTypes = [siteCopy, portfolioPhoto, contentEntry]
+export const schemaTypes = [siteCopy, portfolioPhoto, contentEntry, workEntry, homePage]
