1f7577957560693be5cf846631d8371c0fccf5c4
fromhoyeon <fromhoyeon@gmail.com>
2026-09-05 21:42:35 +0900

    Use 1024 and 2048 photo delivery sizes

--- assets/content/sanity-runtime.js
+++ assets/content/sanity-runtime.js
@@ -199,8 +199,8 @@
       .map((item) => ({
         ...item,
         file: item.filename || item.title || item._id,
-        src: imageUrl(item.url, 1000, 70, 1000),
-        fullSrc: imageUrl(item.url, 1800, 80, 1800)
+        src: imageUrl(item.url, 1024, 70, 1024),
+        fullSrc: imageUrl(item.url, 2048, 80, 2048)
       }));
   }
 
